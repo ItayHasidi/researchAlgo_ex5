c197d543d5af6dc4138c441adf3593cf61f43d61
ItayHasidi <itayhasidi@gmail.com>
2022-11-28 12:49:13 +0200

    update to q1
    infinite loop but correct answers

--- q1.py
+++ q1.py
@@ -12,16 +12,56 @@ class bounded_subsets:
     def __next__(self):
         if sum(self.state) > self.c:
             raise StopIteration
-        if len(self.state) == 0:
-            self.state.append(self.s[0])
-            return self.state
-        else:
-            while len(self.state) > 0:
-                res = self.state.pop(-1)
+            # return self.state
+        # self.state.append(self.s[0])
+        while sum(self.state) <= self.c:
+            if len(self.state) != 0:
+                res = self.state[-1]
                 next_idx = self.s.index(res) + 1
-                if next_idx < len(self.s) and sum(self.state) + self.s[next_idx] <= self.c:
+            else:
+                next_idx = 0
+            if len(self.state) > 0 and self.state[-1] == self.s[-1]:
+                self.state.pop(-1)
+                if len(self.state) == 0:
+                    next_idx = 0
+                else:
+                    res = self.state[-1]
+                    next_idx = self.s.index(res) + 1
+                if next_idx + 2 < len(self.s):
                     self.state.append(self.s[next_idx])
+                    self.state.append(self.s[next_idx + 1])
+                if sum(self.state) <= self.c:
                     return self.state
+
+            if next_idx < len(self.s) and sum(self.state[:-1]) + self.s[next_idx] <= self.c:
+                if len(self.state) != 0:
+                    self.state.pop(-1)
+                self.state.append(self.s[next_idx])
+                return self.state
+                # else:
+
+        # if len(self.state) == 0:
+        #     self.state.append(self.s[0])
+        #     return self.state
+        # else:
+        #
+        #     while len(self.state) > 0 and :
+        #         res = self.state[-1]
+        #         next_idx = self.s.index(res) + 1
+        #         if next_idx < len(self.s) and sum(self.state) + self.s[next_idx] <= self.c:
+        #             self.state.append(self.s[next_idx])
+        #         elif next_idx < len(self.s) and sum(self.state[:-1]) + self.s[next_idx] <= self.c:
+        #             self.state.pop(-1)
+        #             self.state.append(self.s[next_idx])
+        #         return self.state
+
+
+# while len(self.state) > 0:
+#                 res = self.state.pop(-1)
+#                 next_idx = self.s.index(res) + 1
+#                 if next_idx < len(self.s) and sum(self.state) + self.s[next_idx] <= self.c:
+#                     self.state.append(self.s[next_idx])
+#                     return self.state
 
 
 if __name__ == '__main__':
